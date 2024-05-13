d15a4c4ecf81c94e7c11962f882427b29b44e054
LAB14_USER ON WS25 <L14X15W25@citulab.net>
2024-05-13 16:00:50 +0800

    added map

--- smol_projects/Capstone2/core/src/com/mygdx/game/map/Map.java
+++ smol_projects/Capstone2/core/src/com/mygdx/game/map/Map.java
@@ -19,57 +19,9 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
 public class Map {
-    private static final String TAG = Map.class.getSimpleName();
-    private final TiledMap tiledMap;
+    private TiledMap map;
+    Texture[] test_map_textures;
+    OrthogonalTiledMapRenderer tmr;
 
-    public OrthogonalTiledMapRenderer tmr;
-   // private final Array<CollisionArea> collisionAreas;
-    private final Vector2 startLocation;
 
-    public Map(final TiledMap tiledMap) {
-        final MapProperties mapProps = tiledMap.getProperties();
-        this.tiledMap = tiledMap;
-
-      //  this.collisionAreas = new Array<>();
-        //this.camBoundaries = new Array<>();
-        this.startLocation = new Vector2(mapProps.get("playerStartTileX", 0f, Float.class), mapProps.get("playerStartTileY", 0f, Float.class));
-
-        parseCollision();
-
-    }
-    private void parseCollision() {
-        final MapLayer collisionLayer = tiledMap.getLayers().get("collision");
-        if (collisionLayer == null) {
-            Gdx.app.log(TAG, "Map does not have a layer called 'collision'");
-            return;
-        }
-
-        for (final MapObject mapObj : collisionLayer.getObjects()) {
-            if (mapObj instanceof PolylineMapObject) {
-                final Polyline polyline = ((PolylineMapObject) mapObj).getPolyline();
-           //     collisionAreas.add(new CollisionArea(polyline.getX(), polyline.getY(), polyline.getVertices()));
-            } else if (mapObj instanceof RectangleMapObject) {
-                final Rectangle rect = ((RectangleMapObject) mapObj).getRectangle();
-                final float[] rectVertices = new float[10];
-                // left-bot
-                rectVertices[0] = 0;
-                rectVertices[1] = 0;
-                // left-top
-                rectVertices[2] = 0;
-                rectVertices[3] = rect.height;
-                // right-top
-                rectVertices[4] = rect.width;
-                rectVertices[5] = rect.height;
-                // right-bot
-                rectVertices[6] = rect.width;
-                rectVertices[7] = 0;
-                // left-bot
-                rectVertices[8] = 0;
-                rectVertices[9] = 0;
-           //     collisionAreas.add(new CollisionArea(rect.x, rect.y, rectVertices));
-            } else {
-                Gdx.app.log(TAG, "Unsupported mapObject for collision layer: " + mapObj);
-            }
-        }
-    }
 }
